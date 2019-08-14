f5f77c72e98b9ed984c78aec2f5ed014cb019ef2
Anudeep <anudeepd@nbostech.com>
2019-08-14 13:47:44 +0530

    cjhanges

--- mail.php
+++ mail.php
@@ -20,7 +20,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "care.designingworld@gmail.com";
+        $recipient = "anudeep.gmr@gmail.com";
 
         // Set the email subject.
         $subject = "New Message from $name";
